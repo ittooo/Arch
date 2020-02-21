f7918c12f6bec326fa9c19445d6b4a3c8e297b7a
qwe <chiyuz@qq.com>
2020-02-21 12:39:52 +0800

    fix

--- config.sh
+++ config.sh
@@ -278,7 +278,7 @@ install_desktop(){
                 		break
 			;;
                         "custom")
-				pacman -S breeze breeze-gtk bluedevil drkonqi kactivitymanagerd kde-cli-tools kde-gtk-config kdecoration \
+				pacman -S breeze bluedevil drkonqi kactivitymanagerd kde-cli-tools kde-gtk-config kdecoration \
 				kdeplasma-addons kgamma5 khotkeys kinfocenter kmenuedit knetattach kscreen kscreenlocker ksshaskpass \
 				ksysguard kwallet-pam kwayland-integration kwin kwrited libkscreen libksysguard milou plasma-desktop \
 				plasma-integration plasma-nm plasma-pa plasma-workspace polkit-kde-agent powerdevil sddm-kcm \
